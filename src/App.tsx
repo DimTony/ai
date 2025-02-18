bfe6196a9dd73221ce33c121b4f980cf27090d9f
Dimojiaku Obinna Anthony <dimojiakuo@accessbankplc.com>
2025-02-18 17:54:33 +0100

    6.4.2

--- src/App.tsx
+++ src/App.tsx
@@ -364,7 +364,7 @@ const App = () => {
             {/* Logo and App Name */}
             <div className="flex items-center">
               <span className="text-blue-600 text-xl font-bold">Budi</span>
-              <span className="text-cyan-600 text-sm" >By TonyDim</span>
+              <span className="text-cyan-600 text-xs" >By TonyDim</span>
             </div>
 
             {/* Desktop Navigation */}
